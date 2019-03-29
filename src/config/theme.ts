4aab9ae75b9f3056643f1e09efd411c75b1594a8
Anh Pham <duyanhpham1509@gmail.com>
2019-03-29 16:57:52 +0200

    feat: config material ui theme.

---
+++ src/config/theme.ts
@@ -0,0 +1,18 @@
+import {createMuiTheme} from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#afcdc9",
+      main: "#9bc1bc",
+      dark: "#6c8783",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000"
+    }
+  }
+});
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "whatwg-fetch";
 
+import {MuiThemeProvider} from "@material-ui/core";
+import {theme} from "./config/theme";
+
 import "./index.scss";
 
 import App from "./views/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
